e96527cb3428853dd33effd559c9c17e275f9168
Rahmat97 <rhidayat844@gmail.com>
2017-04-01 21:53:37 +0700

    Posttest 5 Selesai

--- app/jadwal_matakuliah.php
+++ app/jadwal_matakuliah.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class jadwal_matakuliah extends Model
 {
-    protected $table = 'jadwal_matakuliah';
-    protected $fillable = ['matakuliah_id','ruangan_id','dosen_matakuliah_id'];
+    //
+    protected $table='jadwal_matakuliah';// mendeklarasikan tabel jadwal_matakuliah
+    // protected $fillable=['mahasiswa_id','ruangan_id','dosen_matakuiah_id'];
+    public function dosen_matakuliah(){ // fungsi dengan nama dosen_matakuliah
+    	return $this->belongsTo(dosen_matakuliah::class);// memberikan nilai return dari fungsi belongsTo yang merelasikan dosen_matakuliah dengan banyak jadwal_Matakuliah
+    } 
+
+    public function ruangan(){  // fungsi dengan nama ruangan
+		return $this->belongsTo(ruangan::class); // memberikan nilai return dari fungsi belongsTo yang mrelasikan ruangan dengan banyak jadwal_matakuliah
+    }
+
+    public function mahasiswa(){ // fungsi dengan nama mahasiswa
+    	return $this->belongsTo(mahasiswa::class); // memberikan nilai return dari fungsi belongsTo yang merelasikan banyak jadwal_matakuliah dengan mahasiswa
+    }
 }
